db935e9c3e314d79e7df155848205e065d7f1ea5
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-17 20:04:22 -0300

    Capítulo 03 - Aula 05: Mapeando relacionamentos

--- src/main/java/jeffersonrolino/com/github/Main.java
+++ src/main/java/jeffersonrolino/com/github/Main.java
@@ -1,6 +1,7 @@
 package jeffersonrolino.com.github;
 
 import jakarta.persistence.EntityManager;
+import jeffersonrolino.com.github.dao.CategoriaDAO;
 import jeffersonrolino.com.github.dao.ProdutoDAO;
 import jeffersonrolino.com.github.entities.Categoria;
 import jeffersonrolino.com.github.entities.Produto;
@@ -10,16 +11,21 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
+        Categoria celulares = new Categoria("CELULARES");
+
         Produto celular = new Produto(
-        "Galaxy S6", "Celular moderno da Samsung", new BigDecimal("2000"), Categoria.CELULARES
+        "Galaxy S6", "Celular moderno da Samsung", new BigDecimal("2000"), celulares
         );
 
         EntityManager entityManager = JPAUtil.getEntityManager();
 
+        CategoriaDAO categoriaDAO = new CategoriaDAO(entityManager);
         ProdutoDAO produtoDao = new ProdutoDAO(entityManager);
+
 
         entityManager.getTransaction().begin();
 
+            categoriaDAO.cadastrar(celulares);
             produtoDao.cadastrar(celular);
 
         entityManager.getTransaction().commit();
---
+++ src/main/java/jeffersonrolino/com/github/dao/CategoriaDAO.java
@@ -0,0 +1,16 @@
+package jeffersonrolino.com.github.dao;
+
+import jakarta.persistence.EntityManager;
+import jeffersonrolino.com.github.entities.Categoria;
+
+public class CategoriaDAO {
+    private EntityManager entityManager;
+
+    public CategoriaDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public void cadastrar(Categoria categoria){
+        this.entityManager.persist(categoria);
+    }
+}
