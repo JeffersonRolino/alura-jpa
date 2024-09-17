b2dc3b08f6a47159127bc2fa7bc82a380a2d8bbb
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-17 19:23:27 -0300

    Capítulo 02 - Aula 05: Persistindo uma entidade

--- src/main/java/jeffersonrolino/com/github/Main.java
+++ src/main/java/jeffersonrolino/com/github/Main.java
@@ -1,7 +1,27 @@
 package jeffersonrolino.com.github;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jeffersonrolino.com.github.entities.Produto;
+
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Produto celular = new Produto();
+        celular.setNome("Galaxy S6");
+        celular.setDescricao("Celular moderno da Samsung");
+        celular.setPreco(new BigDecimal("2000"));
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("loja");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+            entityManager.persist(celular);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 }
